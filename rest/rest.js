4711f0eca3a379632dc20d6eb05d08d77e89f142
NatyaBA <73196083+NatyaBA@users.noreply.github.com>
2021-06-08 23:34:05 +0300

    Add files via upload

--- rest/rest.js
+++ rest/rest.js
@@ -19,6 +19,10 @@ function sendRequest(method, url, post=null) {
         xhr.send(JSON.stringify(post));  //отправляем результат
     })
 }
+const requestUrlPosts = 'https://jsonplaceholder.typicode.com/posts'
+const requestUrlPhotos = 'https://jsonplaceholder.typicode.com/photos'
+const requestUrlComments = 'https://jsonplaceholder.typicode.com/comments'
+const requestUrlAlbums = 'https://jsonplaceholder.typicode.com/albums'
 
 const post =  {
     userId: 3,
@@ -26,96 +30,35 @@ const post =  {
     postId: 2,
     albumId: 5
 }
-
-
-//-----------------Read all post, particular post, create/edit post-------
-function posts() { 
-    const requestUrlPosts = 'https://jsonplaceholder.typicode.com/posts'
+   
+function request(URL) {
     sendRequest();
-    sendRequest('GET', requestUrlPosts)
-        .then (data =>data.forEach(item => {
+    sendRequest('GET', URL)
+        .then (data => data.forEach(item => {        
             let card = document.createElement('div');
             card.classList.add('card');
-            card.innerHTML = `
-             <div class="title">${item.title}</div>
-             <div class="body">${item.body}</div>
-            `;
+           
+            if (item.url) {
+                card.innerHTML = "<div class='title'>" + item.title + "</div><img src=" + item.url + "></div><img src=" + item.thumbnailUrl + "></div>";
+            } 
+            else 
+                if (item.title && item.body) {
+                    card.innerHTML = "<div class='title'>" + item.title + "</div><div>" + item.body + "</div>";                    
+                }
+                else
+                    if (item.name)
+                        card.innerHTML = "<div class='title'>" + item.name + "</div><div>" + item.email + "</div><div>" + item.body + "</div>";
+                    else
+                        card.innerHTML = "<div class='title'>" + item.title + "</div>";
+                   
             document.querySelector('.app').appendChild(card);
+            
         }))
-        .catch(err => console.log(err))
-
-
-    sendRequest('POST', requestUrlPosts, post)
-        .then (data =>data.forEach(item => {
-            let card = document.createElement('div');
-            card.classList.add('card');
-            card.innerHTML = `
-            <div class="title">${item.title}</div>
-            <div class="body">${item.body}</div>
-            `;
-            document.querySelector('.app').appendChild(card);
-        }))
-        .catch(err => console.log(err))
-}
-
-
-//-------------------- Read all albums, particular album--------------
-function albums(){
-    const requestUrlAlbums = 'https://jsonplaceholder.typicode.com/albums'
-    sendRequest();
-    sendRequest('GET', requestUrlAlbums)
-        .then (data =>data.forEach(item => {
-            let card = document.createElement('div');
-            card.classList.add('card');
-            card.innerHTML = `
-            <div class="title">${item.title}</div>
-            `;
-            document.querySelector('.app').appendChild(card);
-        }))
-        .catch(err => console.log(err))
-}
-    
-
-//---------------Read all comments for particular post, add comment comment for post
-function comments() {
-    const requestUrlComments = 'https://jsonplaceholder.typicode.com/comments'
-    sendRequest();
-    sendRequest('GET',  requestUrlComments)
-        .then (data => data.forEach(item => {
-            //data.filter(data => data.postId === 2);
-            let card = document.createElement('div');
-            card.classList.add('card');
-            card.innerHTML = `
-             <div class="title">${item.name}</div>
-             <div class="body">${item.email}</div>
-             <div class="body">${item.body}</div>
-            `;
-            document.querySelector('.app').appendChild(card);
-        }))
-        .catch(err => console.log(err))
-    
-    sendRequest('POST', requestUrlComments, comments)
+        .catch(err => console.log(err)) 
+        
+    document.querySelector('.app').innerHTML = '';
+ 
+    sendRequest('POST', URL, post)
         .then (data =>(data))
         .catch(err => console.log(err))
 }
-    
-
-//---------------------Read all photos for particular album----------
-function photos() {
-    const requestUrlPhotos = 'https://jsonplaceholder.typicode.com/photos'
-    sendRequest();
-    sendRequest('GET', requestUrlPhotos)
-        .then (data => data.forEach(item => {
-            //data.filter(data => data.postId === 2);
-            let card = document.createElement('div');
-            card.classList.add('card');
-            card.innerHTML = `
-            <div class="title">${item.title}</div>
-            <img src="${item.url}"></div>
-            <img src="${item.thumbnailUrl}"></div>
-            `;
-            document.querySelector('.app').appendChild(card);
-        }))
-        .catch(err => console.log(err))
-}
-    
