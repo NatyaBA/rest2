2920f959a4705f51acd6f2c67776380a85bedc7a
NatyaBA <73196083+NatyaBA@users.noreply.github.com>
2021-06-16 17:00:25 +0300

    Add files via upload

--- rest/rest.js
+++ rest/rest.js
@@ -30,33 +30,62 @@ const post =  {
     postId: 2,
     albumId: 5
 }
-   
-function request(URL) {
+let i = 1;
+function request(URL, i) {
     sendRequest();
     sendRequest('GET', URL)
-        .then (data => data.forEach(item => {        
-            let card = document.createElement('div');
-            card.classList.add('card');
-           
-            if (item.url) {
-                card.innerHTML = "<div class='title'>" + item.title + "</div><img src=" + item.url + "></div><img src=" + item.thumbnailUrl + "></div>";
-            } 
-            else 
-                if (item.title && item.body) {
-                    card.innerHTML = "<div class='title'>" + item.title + "</div><div>" + item.body + "</div>";                    
-                }
-                else
-                    if (item.name)
-                        card.innerHTML = "<div class='title'>" + item.name + "</div><div>" + item.email + "</div><div>" + item.body + "</div>";
-                    else
-                        card.innerHTML = "<div class='title'>" + item.title + "</div>";
-                   
-            document.querySelector('.app').appendChild(card);
-            
+        .then (data => data.forEach(item => {  
+            let table = document.querySelector('#table');
+            let items = document.querySelectorAll('#pagination li');
+            let pagination = document.querySelector('#pagination');
+            let notesOnPage=30;
+            if ( i <= Math.ceil(data.length / notesOnPage)) {
+                let li = document.createElement('li');
+                li.innerHTML = i;       
+                i++;
+                pagination.appendChild(li);  
+            }
+            for (item of items) {
+                item.addEventListener("click", function() {
+                    let pageNum = +this.innerHTML;
+                    let start = (pageNum - 1) * notesOnPage;
+                    let end = start + notesOnPage;
+                    let notes = data.slice(start, end);
+                
+                    table.innerHTML = '';
+                    for (let note of notes) {
+                        let tr = document.createElement('tr');
+                        table.appendChild(tr);
+                        if (note.url) {
+                            createCell(note.title, tr);
+                            createCell(note.url, tr);          //photos
+                            createCell(note.thumbnailUrl , tr); 
+                        } 
+                        else 
+                            if (note.title && note.body) {
+                                createCell(note.title, tr);   //posts
+                                createCell(note.body, tr);          
+                            }
+                            else
+                                if (note.name) {
+                                    createCell(note.name, tr);   //comments
+                                    createCell(note.body, tr);
+                                    createCell(note.email, tr); 
+                                }    
+                                else
+                                    createCell(note.title, tr);  //albums
+	    	        }
+                });
+            }
+            function createCell(text, tr) {
+                let td = document.createElement('td');
+                td.innerHTML = text;
+                tr.appendChild(td);
+            }
         }))
         .catch(err => console.log(err)) 
         
-    document.querySelector('.app').innerHTML = '';
+   document.querySelector('#pagination').innerHTML = '';
  
     sendRequest('POST', URL, post)
         .then (data =>(data))
